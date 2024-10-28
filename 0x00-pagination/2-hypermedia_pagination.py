f9400a3a90759c0bebb9a169d06ee4eb328ca187
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-28 23:50:18 +0100

    task2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Contains method index_range and
+class Server
+"""
+
+
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+def index_range(page, page_size) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start
+    index and an end index corresponding to the
+    range of indexes to return in a list for
+    those particular pagination parameters
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+
+    return start, end
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        self.dataset()
+
+        if start < end < len(self.__dataset):
+            return self.__dataset[start:end]
+        return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        return {
+            'page_size': page_size if page < total_pages else 0,
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page + 1 < total_pages else None,
+            'previous_page': page - 1 if 0 < page - 1 < total_pages else None,
+            'total_pages': total_pages,
+        }
