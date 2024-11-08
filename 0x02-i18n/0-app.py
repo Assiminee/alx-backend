51b200a559e25152b50e54b0200860295051c697
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-08 23:03:02 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,11 +2,15 @@
 """ Flask application module """
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
     """ Renders 0-index.html file """
     return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
