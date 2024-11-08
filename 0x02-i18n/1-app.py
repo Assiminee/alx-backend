93429f7aa7eaa4a38da0c905823ec9a6b2011c25
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-08 23:11:16 +0100

    app1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Flask application module """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Babel configuration class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """ Renders 0-index.html file """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
